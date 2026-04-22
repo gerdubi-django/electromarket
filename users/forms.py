bb3dbb39965323090a8fba0a41575dcae102e8ab
German Dubienczuk <dubienczukg@gmail.com>
2026-04-22 13:03:16 -0300

    Improve UI contrast, checkout transaction safety, and i18n coverage

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
 from core.forms import BootstrapFormMixin
 
 
@@ -20,6 +22,11 @@ class ProfileForm(BootstrapFormMixin, forms.ModelForm):
     class Meta:
         model = User
         fields = ("first_name", "last_name", "email")
+        labels = {
+            "first_name": _("Nombre"),
+            "last_name": _("Apellido"),
+            "email": _("Email"),
+        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
