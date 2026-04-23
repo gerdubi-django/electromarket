20005c893a970baf104be5fc6c06e2979e042e1f
German Dubienczuk <dubienczukg@gmail.com>
2026-04-23 07:54:17 -0300

    Refactor language switcher to route-based localized reload

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,8 @@
-from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.i18n import i18n_patterns
 
-urlpatterns = [
-    path("i18n/", include("django.conf.urls.i18n")),
-]
-
-urlpatterns += i18n_patterns(
+urlpatterns = i18n_patterns(
     path("django-admin/", admin.site.urls),
     path("", include("core.urls")),
     path("users/", include("users.urls")),
--- core/static/core/js/app.js
+++ core/static/core/js/app.js
@@ -1,1 +1,28 @@
-document.querySelectorAll('.alert').forEach((element) => setTimeout(() => element.remove(), 4000));
+const removeFlashMessages = () => {
+  document.querySelectorAll('.alert').forEach((element) => {
+    setTimeout(() => element.remove(), 4000);
+  });
+};
+
+const initializeLanguageSelector = () => {
+  const selector = document.querySelector('[data-language-selector]');
+
+  if (!selector) {
+    return;
+  }
+
+  selector.addEventListener('change', ({ target }) => {
+    const nextUrl = target.value;
+
+    if (nextUrl) {
+      window.location.assign(nextUrl);
+    }
+  });
+};
+
+const initializeApp = () => {
+  removeFlashMessages();
+  initializeLanguageSelector();
+};
+
+initializeApp();
